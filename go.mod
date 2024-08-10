577ae6564bdd0e6e954bff16da862f936d0ada57
sajadblnyn <sajadbalaniyan.sb@gmail.com>
2024-08-11 01:58:27 +0330

    fix: reading and wrting to file

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
 module github.com/sajadblnyn/go-file-stream
 
-go 1.22.6
+go 1.22.2
--- main.go
+++ main.go
@@ -2,25 +2,27 @@ package main
 
 import (
 	"bytes"
-	"crypto/rand"
 	"encoding/binary"
 	"fmt"
 	"io"
 	"log"
 	"net"
+	"os"
 )
 
 type FileServer struct {
 }
 
 func (fs *FileServer) start() {
-	ln, err := net.Listen("tcp", ":3001")
+	ln, err := net.Listen("tcp", ":3005")
 	if err != nil {
+
 		log.Fatal(err.Error())
 	}
 
 	for {
 		conn, err := ln.Accept()
+
 		if err != nil {
 			log.Fatal(err.Error())
 		}
@@ -37,30 +39,46 @@ func (fs *FileServer) readLoop(conn net.Conn) {
 		binary.Read(conn, binary.LittleEndian, &size)
 
 		n, err := io.CopyN(buff, conn, size)
+
+		fmt.Println(buff.Bytes())
+		if err != nil {
+			log.Fatal(err.Error())
+
+		}
+
+		f, err := os.OpenFile("2.txt", os.O_APPEND|os.O_WRONLY, os.ModeAppend)
+		defer f.Close()
+
 		if err != nil {
 			log.Fatal(err.Error())
 		}
+		_, err = io.Copy(f, buff)
+		if err != nil {
+			log.Fatal(err.Error())
 
-		fmt.Println(buff.Bytes())
+		}
+		// fmt.Println(buff.Bytes())
 		fmt.Printf("received %d bytes over the network \n", n)
 
 	}
 }
 
-func sendFile(size int) error {
-	file := make([]byte, size)
-	_, err := io.ReadFull(rand.Reader, file)
+func sendFile() error {
+	f, err := os.Open("1.txt")
+	defer f.Close()
 	if err != nil {
 		return err
 	}
+	file := make([]byte, 10000000000)
+	m, _ := io.ReadFull(f, file)
 
-	conn, err := net.Dial("tcp", ":3001")
+	conn, err := net.Dial("tcp", ":3005")
 	if err != nil {
 		return err
 	}
-	binary.Write(conn, binary.LittleEndian, int64(size))
+	binary.Write(conn, binary.LittleEndian, int64(m))
 
-	n, err := io.CopyN(conn, bytes.NewReader(file), int64(size))
+	n, err := io.CopyN(conn, bytes.NewReader(file), int64(m))
 	if err != nil {
 		return err
 	}
@@ -70,7 +88,10 @@ func sendFile(size int) error {
 
 func main() {
 	go func() {
-		sendFile(1000)
+		err := sendFile()
+		if err != nil {
+			panic(err)
+		}
 	}()
 	fileServer := &FileServer{}
 	fileServer.start()
